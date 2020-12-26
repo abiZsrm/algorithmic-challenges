af8bbf92c81aed83af8dd78d110c97204ee9e544
Abhishek Madhusudhan <abisrm@outlook.com>
2020-12-26 18:13:52 -0500

    CTCI: Solved 16.20, 16.21

--- Cracking-the-coding-interview/src-solutions/chap16_Moderate/Problem_16_20.java
+++ Cracking-the-coding-interview/src-solutions/chap16_Moderate/Problem_16_20.java
@@ -1,5 +1,11 @@
 package chap16_Moderate;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
 /**
  * T9: On old cell phones, users typed on a numeric keypad and the phone would
  * provide a list of words that matched these numbers. Each digit mapped to a set of 0-4 
@@ -12,5 +18,102 @@ package chap16_Moderate;
  */
 public class Problem_16_20
 {
-
+	public static void main(String[] args) 
+	{
+		List<String> givenWords = Arrays.asList("tree", "used", "bads"); 
+		String digitSequence = "8733"; 
+		
+		collectValidWordsForSequence(givenWords, digitSequence);
+	}
+	
+	public static void collectValidWordsForSequence(List<String> givenWords, String digitSequence)
+	{
+		List<String> validWordsForSequence = new ArrayList<String>(); 
+		
+		givenWords.stream()
+				  .filter((String input) -> { return isWordForNumberExists(input, digitSequence); })
+				  .forEach(validWordsForSequence::add);
+		
+		System.out.println("Valid words for sequence: " + digitSequence + " are - ");
+		validWordsForSequence.forEach(System.out::println);
+	}
+	
+	public static boolean isWordForNumberExists(String word, String digitSequence)
+	{
+		HashMap<Character, HashSet<Character>> phonePad = retrievePhoneKey(); 
+		
+		for(int i = 0; i < word.length(); i++)
+		{
+			if(!phonePad.get(digitSequence.charAt(i)).contains(Character.valueOf(word.charAt(i))))
+			{
+				return false; 
+			}
+		}
+		
+		return true; 
+	}
+	
+	public static HashMap<Character, HashSet<Character>> retrievePhoneKey()
+	{
+		HashMap<Character, HashSet<Character>> hmap = new HashMap<Character, HashSet<Character>>(); 
+		
+		HashSet<Character> hset1 = new HashSet<Character>(); 
+		
+		HashSet<Character> hset2 = new HashSet<Character>(); 
+		hset2.add('a'); 
+		hset2.add('b'); 
+		hset2.add('c'); 
+		
+		HashSet<Character> hset3 = new HashSet<Character>(); 
+		hset3.add('d'); 
+		hset3.add('e'); 
+		hset3.add('f'); 
+		
+		HashSet<Character> hset4 = new HashSet<Character>(); 
+		hset4.add('g'); 
+		hset4.add('h'); 
+		hset4.add('i'); 
+		
+		HashSet<Character> hset5 = new HashSet<Character>(); 
+		hset5.add('j'); 
+		hset5.add('k'); 
+		hset5.add('l'); 
+		
+		HashSet<Character> hset6 = new HashSet<Character>(); 
+		hset6.add('m'); 
+		hset6.add('n'); 
+		hset6.add('o'); 
+		
+		HashSet<Character> hset7 = new HashSet<Character>(); 
+		hset7.add('p'); 
+		hset7.add('q'); 
+		hset7.add('r'); 
+		hset7.add('s'); 
+		
+		HashSet<Character> hset8 = new HashSet<Character>(); 
+		hset8.add('t'); 
+		hset8.add('u'); 
+		hset8.add('v'); 
+		
+		HashSet<Character> hset9 = new HashSet<Character>(); 
+		hset9.add('w'); 
+		hset9.add('x'); 
+		hset9.add('y'); 
+		hset9.add('z'); 
+		
+		HashSet<Character> hset0 = new HashSet<Character>();  
+		
+		hmap.put('0', hset0); 
+		hmap.put('1', hset1); 
+		hmap.put('2', hset2); 
+		hmap.put('3', hset3); 
+		hmap.put('4', hset4); 
+		hmap.put('5', hset5); 
+		hmap.put('6', hset6); 
+		hmap.put('7', hset7); 
+		hmap.put('8', hset8); 
+		hmap.put('9', hset9); 
+		
+		return hmap; 
+	}
 }
--- Cracking-the-coding-interview/src-solutions/chap16_Moderate/Problem_16_21.java
+++ Cracking-the-coding-interview/src-solutions/chap16_Moderate/Problem_16_21.java
@@ -1,5 +1,7 @@
 package chap16_Moderate;
 
+import java.util.Arrays;
+
 /**
  * Sum Swap: Given two arrays of integers, find a pair of values (one value from each array) that 
  * you can swap to give the two arrays the same sum. 
@@ -11,5 +13,76 @@ package chap16_Moderate;
  */
 public class Problem_16_21
 {
-
+	public static void main(String[] args) 
+	{
+		int[] inputArr1 = new int[] {4, 1, 2, 1, 1, 2}; 
+		int[] inputArr2 = new int[] {3, 6, 3, 3 }; 
+		
+		int[] pairs = sumSwap(inputArr1, inputArr2); 
+		System.out.println(pairs[0]);
+		System.out.println(pairs[1]);
+	}
+	
+	public static int calculateSum(int[] inputArray)
+	{
+		int totalSum = 0; 
+		
+		for(int i = 0; i < inputArray.length; i++)
+		{
+			totalSum += inputArray[i]; 
+		}
+		
+		return totalSum; 
+	}
+	
+	public static int[] sumSwap(int[] inputArr1, int[] inputArr2)
+	{
+		int[] largeArr = null; 
+		int[] smallArr = null; 
+		
+		int arr1sum = calculateSum(inputArr1); 
+		int arr2sum = calculateSum(inputArr2); 
+		
+		if(arr1sum > arr2sum)
+		{
+			largeArr = inputArr1;
+			smallArr = inputArr2; 
+		}
+		else
+		{
+			smallArr = inputArr1; 
+			largeArr = inputArr2; 
+		}
+		
+		int diff = Math.abs(arr1sum - arr2sum); 
+		int foundPairDiff = diff/2; 
+		
+		// Sort both the arrays. 
+		Arrays.sort(inputArr1);
+		Arrays.sort(inputArr2);
+		
+		int i = 0; 
+		int j = 0; 
+		
+		int[] pairs = new int[2]; 
+		while (i < largeArr.length && j < smallArr.length)
+		{
+			if(Math.abs(largeArr[i] - smallArr[j]) == foundPairDiff)
+			{
+				pairs[0] = largeArr[i]; 
+				pairs[1] = smallArr[j]; 
+				return pairs; 
+			}
+			else if ((largeArr[i] - smallArr[j]) > foundPairDiff)
+			{
+				j++; 
+			}
+			else if((largeArr[i] - smallArr[j]) < foundPairDiff)
+			{
+				i++; 
+			}
+		}
+		
+		return pairs; 
+	}
 }
